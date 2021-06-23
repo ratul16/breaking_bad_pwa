3c75d9d4d72087078ee54c49d74d16f437b18b38
Md. Hasibul Alam Ratul <hasibul.ratul@northsouth.edu>
2021-06-23 17:05:45 +0600

    pwa config modified

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,18 @@ module.exports = {
             navigateFallback: '/index.html',
             runtimeCaching: [
                 {
-                    urlPattern: new RegExp('^https://www.breakingbadapi.com/api/'),
+                    urlPattern: new RegExp('^https://www.breakingbadapi.com/api/characters'),
+                    handler: 'networkFirst',
+                    options: {
+                        networkTimeoutSeconds: 20,
+                        cacheName: 'app-cache1',
+                        cacheableResponse: {
+                            statuses: [0, 200]
+                        }
+                    }
+                }
+                {
+                    urlPattern: new RegExp('^https://www.breakingbadapi.com/api/quote/'),
                     handler: 'networkFirst',
                     options: {
                         networkTimeoutSeconds: 20,
